ff229899bf110982a19782878f32e38ab906e272
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-13 21:32:58 +0100

    refact app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
+import 'reflect-metadata';
 import express, { Express } from 'express';
+import { db } from './database';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+db.initialize()
+  .then(() => {
+    console.log('Database initialized');
+  })
+  .catch((err) => {
+    console.log('Database initialization failed', err);
+  });
 
 import { router } from './routes';
 const app: Express = express();
@@ -10,7 +20,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(router);
 
 app.listen(port, () => {
