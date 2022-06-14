f33ca8c6adf530c98a7438bd0fdaff35ea0c03ac
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-14 20:44:11 +0100

    refact routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
-import { MeasurementsController } from './controllers/MeasurementsController';
+import MeasurementsController from './controllers/MeasurementsController';
 import { SmartMeterscontroler } from './controllers/SmartMetersController';
 
 const router = Router();
 
-const smartMetersControler = new SmartMeterscontroler()
-const measurementsController = new MeasurementsController();
+const smartMetersControler = new SmartMeterscontroler();
 
-router.get('/api/v1/meterdata/measurement', measurementsController.getMeasurementsByDay.bind(measurementsController));
-router.post('/api/v1/meterdata/measurement', measurementsController.insertMeasurements.bind(measurementsController));
+router.get('/api/v1/meterdata/measurement', MeasurementsController.getMeasurementsByDay);
+router.post('/api/v1/meterdata/measurement', MeasurementsController.insertMeasurements);
 
-router.get("/api/v1/smartmeters", smartMetersControler.getSmartMeters.bind(smartMetersControler));
-router.post("/api/v1/smartmeters", smartMetersControler.createSmartMeter.bind(smartMetersControler));
+router.get('/api/v1/smartmeters', smartMetersControler.getSmartMeters.bind(smartMetersControler));
+router.post('/api/v1/smartmeters', smartMetersControler.createSmartMeter.bind(smartMetersControler));
 
 export { router };
