7ec9d3e8110027de327b4d5c16566cc8a3c4db4c
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-14 20:54:42 +0100

    add smart meters service

---
+++ src/services/SmartMetersService.ts
@@ -0,0 +1,27 @@
+import { db } from '../database';
+import { SmartMeter } from '../models/SmartMeter.entity';
+
+const createSmartMeter = async () => {
+  try {
+    const created = await db.getRepository(SmartMeter).create();
+    const result = await db.getRepository(SmartMeter).save(created);
+
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw Error('Error creating smart meter');
+  }
+};
+
+const getSmartMeters = async () => {
+  try {
+    const smartMeters = await db.getRepository(SmartMeter).find();
+
+    return smartMeters;
+  } catch (e) {
+    console.log(e);
+    throw Error('Error getting smart meters');
+  }
+};
+
+export default { createSmartMeter, getSmartMeters };
