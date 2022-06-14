b4bce02c88a50f6171a93f7a9793bf239cb17c5c
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-14 20:54:32 +0100

    refact routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,13 @@
 import { Router } from 'express';
 import MeasurementsController from './controllers/MeasurementsController';
-import { SmartMeterscontroler } from './controllers/SmartMetersController';
+import SmartMetersController from './controllers/SmartMetersController';
 
 const router = Router();
-
-const smartMetersControler = new SmartMeterscontroler();
 
 router.get('/api/v1/meterdata/measurement', MeasurementsController.getMeasurementsByDay);
 router.post('/api/v1/meterdata/measurement', MeasurementsController.insertMeasurements);
 
-router.get('/api/v1/smartmeters', smartMetersControler.getSmartMeters.bind(smartMetersControler));
-router.post('/api/v1/smartmeters', smartMetersControler.createSmartMeter.bind(smartMetersControler));
+router.get('/api/v1/smartmeters', SmartMetersController.getSmartMeters);
+router.post('/api/v1/smartmeters', SmartMetersController.createSmartMeter);
 
 export { router };
