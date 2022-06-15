bc4f99e66a691a9c98a9aa4196825ae2074e7cf4
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-15 16:51:35 +0100

    add date validation to measurements controller

--- src/controllers/MeasurementsController.ts
+++ src/controllers/MeasurementsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import dayjs from 'dayjs';
 import MeasurementsService from '../services/MeasurementsService';
 
 const insertMeasurements = async (req: Request, res: Response) => {
@@ -15,40 +16,33 @@ const insertMeasurements = async (req: Request, res: Response) => {
 const getMeasurementsByDay = async (req: Request, res: Response) => {
   const { muid, start, stop } = req.query;
   if (!muid) {
-    res.status(400).json({ error: 'Missing muid parameter' });
+    return res.status(400).json({ error: 'Missing muid parameter' });
   }
 
   if (!start) {
-    res.status(400).json({ error: 'Missing start parameter' });
+    return res.status(400).json({ error: 'Missing start parameter' });
   }
 
-  if (!isDate(start as string)) {
-    res.status(400).json({ error: 'Invalid start parameter' });
+  if (dayjs(start as string).isValid()) {
+    return res.status(400).json({ error: 'Invalid start parameter' });
   }
 
   if (!stop) {
-    res.status(400).json({ error: 'Missing stop parameter' });
+    return res.status(400).json({ error: 'Missing stop parameter' });
   }
 
-  if (!isDate(stop as string)) {
-    res.status(400).json({ error: 'Invalid stop parameter' });
+  if (dayjs(stop as string).isValid()) {
+    return res.status(400).json({ error: 'Invalid stop parameter' });
   }
 
   try {
     const measurements = await MeasurementsService.getMeasurementsByDay(muid as string, start as string, stop as string);
 
-    res.json(measurements);
+    return res.json(measurements);
   } catch (e) {
     console.log(e);
-    res.status(500).send();
+    return res.status(500).send();
   }
-};
-
-const isDate = (date: string) => {
-  console.log('debug: ', date);
-  console.log(Date.parse(date));
-  console.log('debug -------------------------------');
-  return !isNaN(Date.parse(date));
 };
 
 export default { insertMeasurements, getMeasurementsByDay };
